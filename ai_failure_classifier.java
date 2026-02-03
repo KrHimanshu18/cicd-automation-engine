3b9b04f6e1c36dca21f9ec53b0523ce1190dad27
Hrishi-Shekhar <hrishi08072005@gmail.com>
2026-02-03 12:43:15 +0530

    Key Module 1 updated: 03/02/26

--- ai_failure_classifier.java
+++ ai_failure_classifier.java
@@ -2,6 +2,15 @@ class AIFailureClassifier {
 
     public String classifyFailure(String log) {
 
+        if (log.contains("BUILD FAILED")) {
+            return "Build Failure";
+        } 
+        else if (log.contains("TEST FAILED")) {
+            return "Test Failure";
+        } 
         
+        else {
+            return "Unknown Failure";
+        }
     }
 }
