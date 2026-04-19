6f16f5679fadccfb81927ca02dab68fdeb88da1b
ishwariumak2006-6 <ishwariumak2006@gmail.com>
2026-04-19 23:02:10 +0530

    codes updated

--- utils/executor.js
+++ utils/executor.js
@@ -2,7 +2,7 @@ const { exec } = require("child_process");
 
 function runCommand(command, cwd = null) {
     return new Promise((resolve, reject) => {
-        exec(command, { cwd, timeout: 60000 }, (error, stdout, stderr) => {
+        exec(command, { cwd, timeout: 6000 }, (error, stdout, stderr) => {
 
             const output = (stdout || "") + "\n" + (stderr || "");
 
